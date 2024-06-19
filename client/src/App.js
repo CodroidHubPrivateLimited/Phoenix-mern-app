bca41de5d395a1a27852c0fd59e2b0ced7022c0a
Jyotsan HAMAL <hamaljyotsan@gmail.com>
2024-06-19 21:43:25 +0545

    folder patched

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import NavBar from '../src/compoents/users/navigation/navbar';
 import ConditionalRoute from "./routes/conditionalRouting";
-
+import UseFooter from "./compoents/users/footer/footer"
 
 function App() {
   return (
