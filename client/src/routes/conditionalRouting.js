0f4c32af3ab0ab4bbde93c087965ee8e6dc0645e
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2024-06-19 20:47:29 +0545

    added about us pages inside components folder

--- client/src/routes/conditionalRouting.js
+++ client/src/routes/conditionalRouting.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/users/01-home"
+import About from '../compoents/about/about';
+
 
 
 function ConditionalRoute() {
@@ -8,6 +10,7 @@ function ConditionalRoute() {
     return (<>
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path="/about" element={<About />}/>
         </Routes>
     </>)
 
