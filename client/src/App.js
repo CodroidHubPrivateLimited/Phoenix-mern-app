4658d3cf04f7168595be97b2b1d5e2f5a0317669
Nik-doid <nikesh.shrestha2022@pcmgmt.edu.np>
2024-06-19 20:49:11 +0545

    footer added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import NavBar from '../src/compoents/users/navigation/navbar';
 import ConditionalRoute from "./routes/conditionalRouting";
+import Footer from "../src/components/footer/footer.js";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 function App() {
@@ -11,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/*' element={<ConditionalRoute />} />
       </Routes>
-      <UseFooter/>
+      <Footer/>
     </>
   );
 }
