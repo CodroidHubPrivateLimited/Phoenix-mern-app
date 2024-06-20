30ea2d2b8a3feb0531b8f442e256d20fed25fd09
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-06-20 09:59:40 +0545

    feature/scholarshipsUI

--- client/src/routes/conditionalRouting.js
+++ client/src/routes/conditionalRouting.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/users/01-home"
+import Scholarships from "../compoents/users/homePageComponents/03-scholarships"
 
 
 function ConditionalRoute() {
@@ -8,6 +9,7 @@ function ConditionalRoute() {
     return (<>
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/Scholarships' element={<Scholarships/>} />
         </Routes>
     </>)
 
