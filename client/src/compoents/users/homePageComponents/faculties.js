6b0a4e279cdbf5c1a87a0e279ff84fa751a0e9f2
Bijaypaudel977 <paudelbijay9844@gmail.com>
2024-06-20 18:31:30 +0545

    faculties-by-bijay-paudel

---
+++ client/src/compoents/users/homePageComponents/faculties.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "../../../App.css";
+
+function Faculties() {
+  return (
+    <>
+      <div class="top-main-container">
+        <div class="faculty-heading-container">
+          <div>
+            <p class="provided-faculty-heading">Faculties Provided</p>
+          </div>
+          <div>
+            <p class="desciption">
+              Kubernetes Operators are application-specific controllers that
+              extend the Kubernetes API to create, configure, and manage
+              instances of stateful applications such as databases. Using the
+              MongoDB Kubernetes operator, you have full control over your
+              MongoDB deployment from a single Kubernetes control plane, with a
+              consistent experience across different deployment environments.
+            </p>
+          </div>
+        </div>
+        <div class="faculty-main-container1">
+          <div class="faculty-container">
+            <h3>Faculties</h3>
+            {/* <img class="faculty-image-container" src={image5} alt="faculty" /> */}
+            <a href="#" class="see-more-link">See more</a>
+          </div>
+
+          <div class="faculty-container">
+          <h3>Faculties</h3>
+              {/* <img class="faculty-image-container" src={image2} alt="faculty" /> */}
+            <a href="#" class="see-more-link">See more</a>
+          </div>
+          <div class="faculty-container">
+            <h3>Faculties</h3>
+            {/* <img class="faculty-image-container" src={image3} alt="faculty" /> */}
+            <a href="#" class="see-more-link">See more</a>
+          </div>
+          <div class="faculty-container">
+            <h3>Faculties</h3>
+
+            {/* <img class="faculty-image-container" src={image4} alt="faculty" /> */}
+
+            <a href="#" class="see-more-link">See more</a>
+          </div>
+        </div>
+        <div class="faculty-main-link-container">
+          <a class="faculty-link" href="#">
+            See more
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Faculties;
--- client/src/pages/users/01-home.js
+++ client/src/pages/users/01-home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutUs from '../../compoents/users/homePageComponents/02-aboutUs'
 import Scholarships from '../../compoents/users/homePageComponents/03-scholarships'
+import Faculties from '../../compoents/users/homePageComponents/faculties'
 
 function Home() {
     return (
         <div>
             <AboutUs />
             <Scholarships />
+            <Faculties/>
         </div>
     )
 }
