33eeb4fdd436cd2dd39be3a6220f3fb78b3d7362
Sakshyam <sakshyam022@gmail.com>
2024-06-22 20:28:46 +0545

    Signup page

---
+++ client/src/compoents/admin/forms/sign_in.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+
+    try {
+      const response =axios.post('http://localhost:8000/signin', {
+        email,
+        password
+      })
+    } catch (error) {
+      console.log(error);
+    }
+
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input 
+            type="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            required 
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required 
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
